f7c129748430aea0f52c749991af53273d74a069
Nikkhil <muthye.n@husky.neu.edu>
2015-11-05 15:35:34 -0500

    Updating profile css

--- public/project/service/StoryService.js
+++ public/project/service/StoryService.js
@@ -7,7 +7,7 @@
 
     function StoryService() {
         var stories = [
-        {heading: "ff", contents: "ff", id: "4cbe04f5-3463-95e7-a1ab-62b7a11cfd30", userId: "9843473b-d068-104e-e46-f623566a5c61"}
+        {heading: "Carrick defends Van Gaal: I'll take winning over tactics", contents: "ff", id: "4cbe04f5-3463-95e7-a1ab-62b7a11cfd30", userId: "9843473b-d068-104e-e46-f623566a5c61"}
         ];
 
      	function guid() 
